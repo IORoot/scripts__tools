db69ff4d85c0bc2a183b15e6ae53aff7f5bcdcbf
Andy Pearson <andy@londonparkour.com>
2021-06-20 19:49:46 +0100

    Add scripts

--- scripts/zsh/symlinks.sh
+++ scripts/zsh/symlinks.sh
@@ -2,20 +2,20 @@
 
 # help 
 # command lister tool
-ln -sf ~/scripts_tools/scripts/command_lister/help.sh /usr/local/bin/help
+ln -sf ~/scripts__tools/scripts/command_lister/help.sh /usr/local/bin/help
 
 # lsg
 # ls-git shows the current git status recursively in all subfolders.
-ln -sf ~/scripts_tools/scripts/lsg/lsg /usr/local/bin/lsg
+ln -sf ~/scripts__tools/scripts/lsg/lsg /usr/local/bin/lsg
 
 # img2mp4
 # uses FFMPEG to loop an image for 5 seconds
-ln -sf ~/scripts_tools/scripts/ffmpeg/img2mp4 /usr/local/bin/img2mp4
+ln -sf ~/scripts__tools/scripts/ffmpeg/img2mp4 /usr/local/bin/img2mp4
 
 # logstats
 # helps diagnose IP addresses in an apache log file
-ln -sf ~/scripts_tools/scripts/log_stats/log_stats /usr/local/bin/log_stats
+ln -sf ~/scripts__tools/scripts/log_stats/log_stats /usr/local/bin/log_stats
 
 # logstats
 # helps diagnose IP addresses in an apache log file
-ln -sf ~/scripts_tools/scripts/log_stats/log_stats /usr/local/bin/log_stats
+ln -sf ~/scripts__tools/scripts/log_stats/log_stats /usr/local/bin/log_stats
