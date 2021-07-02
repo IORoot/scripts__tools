3de009d54d8f90bcee0b028ab7c83f4c68b77e1e
Andy Pearson <andy@londonparkour.com>
2021-07-02 19:09:53 +0100

    SEARCH

--- scripts/deploy_v2/deploy_site.sh
+++ scripts/deploy_v2/deploy_site.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 source cli_colours.sh
 
-    WP_PATH=`./find_wp_instance.sh $1`
-
-    echo $WP_PATH
+    SEARCH=$1
+    
+    WP_PATH=`./find_wp_instance.sh $SEARCH`
 
     WP_VARS=`./get_db_details.sh $WP_PATH`
-
-    echo $WP_VARS
 
     ./dump_db.sh $WP_VARS
