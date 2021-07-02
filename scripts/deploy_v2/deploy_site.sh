5490478933687a8a7e661c4f87e681c8251f1037
Andy Pearson <andy@londonparkour.com>
2021-07-02 19:07:13 +0100

    depl

--- scripts/deploy_v2/deploy_site.sh
+++ scripts/deploy_v2/deploy_site.sh
@@ -5,4 +5,12 @@ source cli_colours.sh
     DB_USER="two"
     DB_PASS="thrreeeee"
 
-    ./dump_db.sh
+    WP_PATH=`./find_wp_instance.sh`
+
+    echo $WP_PATH
+
+    WP_VARS=`./get_db_details.sh $WP_PATH`
+
+    echo $WP_VARS
+
+    ./dump_db.sh $WP_VARS
