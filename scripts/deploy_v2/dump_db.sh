700d836f958deef6e48411e7a0806ca6ebb6bfa4
Andy Pearson <andy@londonparkour.com>
2021-07-02 18:46:47 +0100

    check

--- scripts/deploy_v2/dump_db.sh
+++ scripts/deploy_v2/dump_db.sh
@@ -5,16 +5,19 @@ source cli_colours.sh
 override_env_variables(){
 
     echo "one=$1\n"
+    echo "two=$2\n"
+    echo "three=$3\n"
+    echo "all=$@"
+    echo "PIPE=$PIPE"
+
     if [[ -n "$1" ]];then
         DB_NAME=$1
     fi
 
-    echo "two=$2\n"
     if [[ -n "$2" ]];then
         DB_USER=$2
     fi
 
-    echo "three=$3\n"
     if [[ -n "$3" ]];then
         DB_PASS=$3
     fi
@@ -37,12 +40,9 @@ check_parameters(){
 if [[ -p /dev/stdin ]]; then
     PIPE=$(cat -)
 fi
+# All
+ARGS=$@
 
-echo "one=$1\n"
-echo "two=$2\n"
-echo "three=$3\n"
-echo "all=$@"
-echo "PIPE=$PIPE"
 # Accept ALL Arguments
-override_env_variables
+override_env_variables $PIPE $ARGS
 check_parameters
