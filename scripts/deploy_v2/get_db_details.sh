e495af3ed4a2154905eb687f4690fe9ef899f415
Andy Pearson <andy@londonparkour.com>
2021-07-02 17:51:05 +0100

    redo

--- scripts/deploy_v2/get_db_details.sh
+++ scripts/deploy_v2/get_db_details.sh
@@ -2,33 +2,28 @@
 
 source cli_colours.sh
 
-# Sets the ENV Variables.
-if [ "$#" -ne 1 ]; then
-    echo -e "$0 [DIR] - ${Orange} WordPress directory that contains wp-config.php"
-    exit 1
+if [ $# -eq 0 ] ; then
+    read_wpconfig_variables < /dev/stdin
+else
+    read_wpconfig_variables $1
 fi
-
-WPPATH=$1
-
-move_into_dir(){
-    if [ -d "${WPPATH}" ] 
-    then
-        cd $WPPATH
-    else
-        echo "Error: Directory ${WPPATH} does not exist."
-        exit
-    fi
-}
 
 
 # Read the Wordpress config variables from wp-config
 read_wpconfig_variables() {
-    export WP_DBNAME=`cat $WPPATH/wp-config.php | grep "DB_NAME" | cut -d \' -f 4`
-    export WP_DBUSER=`cat $WPPATH/wp-config.php | grep "DB_USER" | cut -d \' -f 4`
-    export WP_DBPASS=`cat $WPPATH/wp-config.php | grep "DB_PASSWORD" | cut -d \' -f 4`
+    WP_PATH=$1
 
-    printf "${NC}Database: ${Orange} %-30s ${NC}| Username: ${Cyan} %-20s ${NC}| Password: ${Red} %-20s \n" "${WP_DBNAME}" "${WP_DBUSER}" "${WP_DBPASS}" 
+    if [ -d "${WP_PATH}" ] 
+    then
+        cd $WP_PATH
+    else
+        echo "Error: Directory ${WP_PATH} does not exist."
+        exit
+    fi
+
+    export WP_DBNAME=`cat $WP_PATH/wp-config.php | grep "DB_NAME" | cut -d \' -f 4`
+    export WP_DBUSER=`cat $WP_PATH/wp-config.php | grep "DB_USER" | cut -d \' -f 4`
+    export WP_DBPASS=`cat $WP_PATH/wp-config.php | grep "DB_PASSWORD" | cut -d \' -f 4`
+
+    printf "${NC}Database:${Orange} %-30s ${NC}| Username:${Cyan} %-20s ${NC}| Password:${Red} %-20s \n" "${WP_DBNAME}" "${WP_DBUSER}" "${WP_DBPASS}"
 }
-
-move_into_dir
-read_wpconfig_variables
