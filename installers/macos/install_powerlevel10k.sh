cab7125b439941844bc7e695ff6289e52f104e7b
Andy Pearson <andypearson@Andys-MacBook-Air.local>
2022-03-20 13:04:40 +0000

    update powerlevel

--- installers/macos/install_powerlevel10k.sh
+++ installers/macos/install_powerlevel10k.sh
@@ -2,13 +2,15 @@
 
 source $INSTALL_DIR/installers/helpers.sh
 
+if [ ! -f "~/.p10k.zsh" ]; then
+    ln -s $INSTALL_DIR/scripts/dotfiles/.p10k.zsh ~/.p10k.zsh
+fi
+
 if [ -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
     printf "Already Installed\n"
 else
     # Clone into custom themes
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-    ln -s $INSTALL_DIR/scripts/dotfiles/.p10k.zsh ~/.p10k.zsh
 
     printf "DONE\n"
 fi
