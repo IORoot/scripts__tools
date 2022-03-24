f2c6f3c483296665de5e0fe7dc69017f9ca6097e
Andy Pearson <andy@londonparkour.com>
2022-03-24 13:50:17 +0000

    Terraform

--- installers/macos/install_hb-terraform.sh
+++ installers/macos/install_hb-terraform.sh
@@ -5,7 +5,7 @@
 
 if [ -f "/usr/local/bin/brew" ]; then
 
-    if [ -d /usr/local/Cellar/terraform ]; then
+    if [ -d /usr/local/Cellar/terraform@0.13 ]; then
         printf "Already Installed\n"
     else
         brew list terraform 1>/dev/null || brew install terraform@0.13
