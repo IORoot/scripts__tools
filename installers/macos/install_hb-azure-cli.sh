d62a3fdfd3deff6718b7a0d09a43e07a4c9fc9ff
Andy Pearson <andy@londonparkour.com>
2022-03-25 11:51:27 +0000

    Azure

---
+++ installers/macos/install_hb-azure-cli.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Dependencies
+# pandoc ✘, rust ✘
+
+if [ -f "/usr/local/bin/brew" ]; then
+
+    if [ -d /usr/local/Cellar/azure-cli ]; then
+        printf "Already Installed\n"
+    else
+        brew list terraform 1>/dev/null || brew install azure-cli
+        printf "DONE\n"
+    fi
+
+else
+    printf "HOMEBREW Not Installed\n"
+fi
