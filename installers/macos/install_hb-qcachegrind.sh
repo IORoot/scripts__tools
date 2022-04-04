956bc723fbe4584391f65d195a1096cdfcc1d9a5
Andy Pearson <andy@londonparkour.com>
2022-04-04 08:33:36 +0100

    qCacheGrind homebrew

---
+++ installers/macos/install_hb-qcachegrind.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Dependencies
+# pandoc ✘, rust ✘
+
+if [ -f "/usr/local/bin/brew" ]; then
+
+    if [ -d /usr/local/Cellar/qcachegrind ]; then
+        printf "Already Installed\n"
+    else
+        brew list wget 1>/dev/null || brew install qcachegrind
+        printf "DONE\n"
+    fi
+
+else
+    printf "HOMEBREW Not Installed\n"
+fi
