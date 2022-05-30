a893469f9d9577a2a9bb077664acfd6b44fa101d
Andy Pearson <andy@londonparkour.com>
2022-05-30 07:57:16 +0100

    yarn

---
+++ installers/macos/install_hb-yarn.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Dependencies
+# pandoc ✘, rust ✘
+
+if [ -f "/usr/local/bin/brew" ]; then
+
+    if [ -d /usr/local/Cellar/yarn ]; then
+        printf "Already Installed\n"
+    else
+        brew list yarn 1>/dev/null || brew install yarn                  #
+        printf "DONE\n"
+    fi
+
+else
+    printf "HOMEBREW Not Installed\n"
+fi
